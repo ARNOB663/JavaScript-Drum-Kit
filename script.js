e318597f4b6b4712a18e5cee27bb08cf84ecd4bd
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2024-11-08 18:52:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-window.addEventListener('keydown', function(e) {
+// window.addEventListener('keydown', function(e) {
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);//selecting the key that was pressed
+// if(!audio) return; //stop the function from running all together
+// audio.currentTime = 0; //rewind to the start
+// audio.play();//play the audio
+// key.classList.add('playing');//add the playing class to the key
+// });
+
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);//selecting the key that was pressed
 if(!audio) return; //stop the function from running all together
 audio.currentTime = 0; //rewind to the start
 audio.play();//play the audio
 key.classList.add('playing');//add the playing class to the key
-});
+
+}
 
 function removeTransition(e){
   if(e.propertyName !== 'transform') return;//skip it if it's not a transform
-  console.log(e.propertyName);
+ this.classList.remove('playing');//remove the playing class from the key
 }
-
 
 //remove the transition effect after the key is pressed 
 const keys = document.querySelectorAll('.key');//select all the keys
 keys.forEach(keys => keys.addEventListener('transitionend', removeTransition));//add event listener to each key
+
+window.addEventListener('keydown', playSound);
+
+
+
